26bdc69f343d42ad6076ce880d87165d5e803533
Christian Flores <104774677+AmbitiousFlo@users.noreply.github.com>
2023-09-12 20:13:36 -0500

    Updated project
    seeds,schema,index,queries

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,7 @@
-DROP DATABASE IF EXISTS employees_db;
-CREATE DATABASE employees_db;
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
 
-USE employees_db;
+USE employee_db;
 
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -22,14 +22,13 @@ CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT ,
+    role_id INT,
     manager_id INT,
     FOREIGN KEY (role_id)
-    REFERENCES  role(id)
+    REFERENCES role(id)
     ON DELETE SET NULL,
 
     FOREIGN KEY (manager_id)
-    REFERENCES  employee(id)
+    REFERENCES employee(id)
     ON DELETE SET NULL
 );
-
